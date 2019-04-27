1c216c856cce78bfdddc3b8fac7f90f8d2948bbe
hu <861956497@qq.com>
2019-04-27 14:38:17 +0800

    第二次提交

--- static/web/js/comm.js
+++ static/web/js/comm.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-	
-	
+	//banner
+    $('#banner').easyFader();
 	
     //nav
 	$("#mnavh").click(function(){
@@ -8,15 +8,30 @@ $(document).ready(function () {
 	$("#mnavh").toggleClass("open");
 	});
 	  
-var obj=null;
-var As=document.getElementById('starlist').getElementsByTagName('a');
-obj = As[0];
-for(i=1;i<As.length;i++){if(window.location.href.indexOf(As[i].href)>=0)
-obj=As[i];}
-obj.id='selected';
-
-	
+	  
+	 //nav 		
+    var obj=null;
+    var As=document.getElementById('starlist').getElementsByTagName('a');
+     obj = As[0];
+     for(i=1;i<As.length;i++){if(window.location.href.indexOf(As[i].href)>=0)
+     obj=As[i];}
+     obj.id='selected';
+   /*
   
+  search
+  
+  */
+    $('.search_ico').click(function () {
+        $('.search_bar').toggleClass('search_open');
+        if ($('#keyboard').val().length > 2) {
+            $('#keyboard').val('');
+            $('#searchform').submit();
+        } else {
+            return false;
+        }
+    });
+	
+    //header
 	var new_scroll_position = 0;
 	var last_scroll_position;
 	var header = document.getElementById("header");
@@ -24,15 +39,11 @@ obj.id='selected';
 	window.addEventListener('scroll', function(e) {
 	  last_scroll_position = window.scrollY;
 
-	  // Scrolling down
 	  if (new_scroll_position < last_scroll_position && last_scroll_position > 80) {
-		// header.removeClass('slideDown').addClass('slideUp');
 		header.classList.remove("slideDown");
 		header.classList.add("slideUp");
 
-	  // Scrolling up
 	  } else if (new_scroll_position > last_scroll_position) {
-		// header.removeClass('slideUp').addClass('slideDown');
 		header.classList.remove("slideUp");
 		header.classList.add("slideDown");
 	  }
@@ -41,34 +52,14 @@ obj.id='selected';
 	});
 	
 	
-	//回到顶部
-    // browser window scroll (in pixels) after which the "back to top" link is shown
-    var offset = 300,
-        //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-        offset_opacity = 1200,
-        //duration of the top scrolling animation (in ms)
-        scroll_top_duration = 700,
-        //grab the "back to top" link
-        $back_to_top = $('.cd-top');
-
-    //hide or show the "back to top" link
-    $(window).scroll(function () {
-        ($(this).scrollTop() > offset) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
-        if ($(this).scrollTop() > offset_opacity) {
-            $back_to_top.addClass('cd-fade-out');
-        }
-    });
-    //smooth scroll to top
-    $back_to_top.on('click', function (event) {
-        event.preventDefault();
-        $('body,html').animate({
-                scrollTop: 0,
-            }, scroll_top_duration
-        );
-    });
 	
-	//侧栏固定
-		   
+	
+		$('.fenlei li').click(function(){
+                $(this).addClass('flselect').siblings().removeClass('flselect');
+                $('.newstw>ul:eq('+$(this).index()+')').show().siblings().hide();
+            });
+
+	
 	//aside
     var Sticky = new hcSticky('aside', {
       stickTo: 'main',
@@ -82,4 +73,27 @@ obj.id='selected';
       }
     });
 	
+	
+	//scroll to top
+    var offset = 300,
+        offset_opacity = 1200,
+        scroll_top_duration = 700,
+        $back_to_top = $('.cd-top');
+
+    $(window).scroll(function () {
+        ($(this).scrollTop() > offset) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+        if ($(this).scrollTop() > offset_opacity) {
+            $back_to_top.addClass('cd-fade-out');
+        }
+    });
+    $back_to_top.on('click', function (event) {
+        event.preventDefault();
+        $('body,html').animate({
+                scrollTop: 0,
+            }, scroll_top_duration
+        );
+    });
+	
+	
+		
 	});
--- web/views.py
+++ web/views.py
@@ -9,3 +9,14 @@ web_blueprint = Blueprint('web', __name__)
 def index():
     return render_template('web/index.html')
 
+@web_blueprint.route('/about/',methods=['GET'])
+def about():
+    return render_template('web/about.html')
+
+@web_blueprint.route('/share/',methods=['GET'])
+def share():
+    return render_template('web/share.html')
+
+@web_blueprint.route('/list/',methods=['GET'])
+def list():
+    return render_template('web/time.html')
